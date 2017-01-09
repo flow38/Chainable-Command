634e8fd0f9bc473c08b4e24a6bcfce7c19ee03bd
Florent Falcy <flow38@gmail.com>
2017-01-09 15:52:37 +0100

    Nuget stuffs!

--- ChainCommand/Properties/AssemblyInfo.cs
+++ ChainCommand/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BaseChainCommand")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A command pattern implementation where commands can be chained to create macro command.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("BaseChainCommand")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("Florent Falcy")]
+[assembly: AssemblyProduct("ChainCommand")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ ChainCommand/publish_nupkg.sh
@@ -0,0 +1,2 @@
+nuget push ./ChainCommand.1.0.0.nupkg -Source  https://www.nuget.org/api/v2/package;
+exit;
